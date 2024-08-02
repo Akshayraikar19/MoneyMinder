dd3af357bd1e601c7a368e8b6c18a6573d1a8b0b
Akshayraikar19 <143585057+Akshayraikar19@users.noreply.github.com>
2024-08-02 18:39:41 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -7,8 +7,8 @@
 import axios from "axios";
 
 // console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL);
-
 export default axios.create({
+    axios.defaults.withCredentials = true
     baseURL: money-minder-backend.vercel.app
 });
 
