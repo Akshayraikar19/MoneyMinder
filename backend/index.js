21976f0e481c136584ef6ac951b6a254b16433fd
Akshayraikar19 <143585057+Akshayraikar19@users.noreply.github.com>
2024-08-02 20:28:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const port = 4444
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://money-minder-backend.vercel.app'],
+    origin: ['http://localhost:3000', 'money-minder-loan-app-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
